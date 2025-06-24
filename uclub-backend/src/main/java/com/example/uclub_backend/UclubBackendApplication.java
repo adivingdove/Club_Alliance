d95d6eb340fd9a8a2cdd4b4b8a6fa95cd9005dbd
adivingdove <m15102363922@163.com>
2025-06-24 10:47:18 +0800

    基础框架提交，前后端分离

---
+++ uclub-backend/src/main/java/com/example/uclub_backend/UclubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.uclub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UclubBackendApplication.class, args);
+	}
+
+}
---
+++ uclub-backend/src/test/java/com/example/uclub_backend/UclubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uclub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UclubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
