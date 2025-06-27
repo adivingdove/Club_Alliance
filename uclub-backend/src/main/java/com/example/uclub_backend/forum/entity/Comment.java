40295efbd1cf58a38f72764260b675f819662482
adivingdove <m15102363922@163.com>
2025-06-27 17:20:51 +0800

    修改comment实体类，添加主键，确保后端可运行

--- uclub-backend/src/main/java/com/example/uclub_backend/forum/entity/Comment.java
+++ uclub-backend/src/main/java/com/example/uclub_backend/forum/entity/Comment.java
@@ -1,19 +1,24 @@
 package com.example.uclub_backend.forum.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-
+@Entity
 @Data
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 自增主键（MySQL/Auto）
     private Long id;
+
     private Long postId;
     private Long userId;
     private String content;
     private CommentStatus status;
+
     @Column(name = "like_count")
     private Integer likeCount;
+
     private LocalDateTime createdAt;
 }
